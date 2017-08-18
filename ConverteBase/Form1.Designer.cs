f5010380cf74efbd22b5a53d4b645bbcf8d3a1bb
Julia Schmidt <juliasn14@gmail.com>
2017-08-17 21:57:40 -0300

    Adicionar arquivos de projeto.

---
+++ ConverteBase/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace ConverteBase
+{
+    partial class JanelaConverte
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConverter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.opcDecimal = new System.Windows.Forms.RadioButton();
+            this.opcOctal = new System.Windows.Forms.RadioButton();
+            this.opcHexa = new System.Windows.Forms.RadioButton();
+            this.opcBinario = new System.Windows.Forms.RadioButton();
+            this.valorInserido = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.Location = new System.Drawing.Point(103, 204);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.Size = new System.Drawing.Size(75, 23);
+            this.btnConverter.TabIndex = 0;
+            this.btnConverter.Text = "Converter";
+            this.btnConverter.UseVisualStyleBackColor = true;
+            this.btnConverter.Click += new System.EventHandler(this.btnConverter_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Valor que deseja converter:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Selecione o tipo de valor inserido:";
+            // 
+            // opcDecimal
+            // 
+            this.opcDecimal.AutoSize = true;
+            this.opcDecimal.Location = new System.Drawing.Point(184, 96);
+            this.opcDecimal.Name = "opcDecimal";
+            this.opcDecimal.Size = new System.Drawing.Size(63, 17);
+            this.opcDecimal.TabIndex = 3;
+            this.opcDecimal.TabStop = true;
+            this.opcDecimal.Text = "Decimal";
+            this.opcDecimal.UseVisualStyleBackColor = true;
+            // 
+            // opcOctal
+            // 
+            this.opcOctal.AutoSize = true;
+            this.opcOctal.Location = new System.Drawing.Point(184, 142);
+            this.opcOctal.Name = "opcOctal";
+            this.opcOctal.Size = new System.Drawing.Size(50, 17);
+            this.opcOctal.TabIndex = 4;
+            this.opcOctal.TabStop = true;
+            this.opcOctal.Text = "Octal";
+            this.opcOctal.UseVisualStyleBackColor = true;
+            // 
+            // opcHexa
+            // 
+            this.opcHexa.AutoSize = true;
+            this.opcHexa.Location = new System.Drawing.Point(184, 165);
+            this.opcHexa.Name = "opcHexa";
+            this.opcHexa.Size = new System.Drawing.Size(86, 17);
+            this.opcHexa.TabIndex = 5;
+            this.opcHexa.TabStop = true;
+            this.opcHexa.Text = "Hexadecimal";
+            this.opcHexa.UseVisualStyleBackColor = true;
+            // 
+            // opcBinario
+            // 
+            this.opcBinario.AutoSize = true;
+            this.opcBinario.Location = new System.Drawing.Point(184, 119);
+            this.opcBinario.Name = "opcBinario";
+            this.opcBinario.Size = new System.Drawing.Size(57, 17);
+            this.opcBinario.TabIndex = 6;
+            this.opcBinario.TabStop = true;
+            this.opcBinario.Text = "Binário";
+            this.opcBinario.UseVisualStyleBackColor = true;
+            // 
+            // valorInserido
+            // 
+            this.valorInserido.Location = new System.Drawing.Point(155, 28);
+            this.valorInserido.Name = "valorInserido";
+            this.valorInserido.Size = new System.Drawing.Size(117, 20);
+            this.valorInserido.TabIndex = 7;
+            // 
+            // JanelaConverte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 239);
+            this.Controls.Add(this.valorInserido);
+            this.Controls.Add(this.opcBinario);
+            this.Controls.Add(this.opcHexa);
+            this.Controls.Add(this.opcOctal);
+            this.Controls.Add(this.opcDecimal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConverter);
+            this.Name = "JanelaConverte";
+            this.Text = "Convertendo a Base";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConverter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton opcDecimal;
+        private System.Windows.Forms.RadioButton opcOctal;
+        private System.Windows.Forms.RadioButton opcHexa;
+        private System.Windows.Forms.RadioButton opcBinario;
+        private System.Windows.Forms.TextBox valorInserido;
+    }
+}
+
---
+++ ConverteBase/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConverteBase
+{
+    public partial class JanelaConverte : Form
+    {
+        public JanelaConverte()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConverter_Click(object sender, EventArgs e)
+        {
+            String valor = valorInserido.Text;
+            //int tamanho = valor.Length;
+            if (opcDecimal.Checked)
+            {
+                double d = Convert.ToDouble(valor);
+                String db = DecimalParaBinario(d);
+                String dO = DecimalParaOctal(d);
+                String dh = DecimalParaHexadecimal(d);
+                MessageBox.Show("Valor Inserido: "+valor+"\nBinário: "+db+"\nOctal: "+dO+"\nHexadecimal: "+dh);
+            }else if(opcBinario.Checked){
+                double bd = BinarioParaDecimal(valor);
+                String dO = DecimalParaOctal(bd);
+                String dh = DecimalParaHexadecimal(bd);
+                MessageBox.Show("Valor Inserido: " + valor + "\nDecimal: " + bd + "\nOctal: " + dO + "\nHexadecimal: " + dh);
+            }else if(opcOctal.Checked){
+                double oD = OctalParaDecimal(valor);
+                String dh = DecimalParaHexadecimal(oD);
+                String db = DecimalParaBinario(oD);
+                MessageBox.Show("Valor Inserido: " + valor + "\nDecimal: " + oD + "\nBinário: " + db + "\nHexadecimal: " + dh);
+            }else if(opcHexa.Checked){
+                double hd = HexadecimalParaDecimal(valor);
+                String dO = DecimalParaOctal(hd);
+                String db = DecimalParaBinario(hd);
+                MessageBox.Show("Valor Inserido: " + valor + "\nDecimal: " + hd + "\nBinario: " + db + "\nOctal: " + dO);
+            }
+            else
+            {
+                MessageBox.Show("Nenhum tipo de base foi selecionado!");
+
+            }
+        }
+
+        private string inverterString(string Word)
+        {
+            char[] arrChar = Word.ToCharArray();
+            Array.Reverse(arrChar);
+            string invertida = new String(arrChar);
+
+            return invertida;
+        }
+
+        public String DecimalParaBinario(double d)
+        {
+            List<int> binarios = new List<int>();
+            double r;
+
+            while (d != 0)
+            {
+                r = d % 2;
+                d = (int)d / 2;
+                binarios.Add(Convert.ToInt32(r));
+            }
+            binarios.Reverse();
+
+            StringBuilder builder = new StringBuilder();
+
+            foreach (int n in binarios)
+            {
+                builder.Append(n);
+            }
+            string result = builder.ToString();
+
+            return result;
+        }
+
+
+        public String DecimalParaOctal(double d)
+        {
+            List<int> octal = new List<int>();
+            double r;
+
+            do
+            {
+                r = d % 8;
+                d = (int)d / 8;
+                octal.Add(Convert.ToInt32(r));
+                if (d < 8)
+                {
+                    octal.Add(Convert.ToInt32(d));
+                }
+            } while (d >= 8);
+
+            octal.Reverse();
+
+            StringBuilder builder = new StringBuilder();
+            foreach (int n in octal)
+            {
+                builder.Append(n);
+            }
+            string result = builder.ToString();
+            return result;
+        }
+
+        public String DecimalParaHexadecimal(double d)
+        {
+            List<int> hexa = new List<int>();
+            double r;
+
+            do
+            {
+                r = d % 16;
+                d = (int)d / 16;
+                hexa.Add((int)r);
+            } while (d >= 16);
+
+            List<string> listaHexa = new List<string>();
+
+            listaHexa = ConverterEmHexadecimal(hexa);
+            listaHexa.Reverse();
+            StringBuilder builder = new StringBuilder();
+            foreach(string s in listaHexa)
+            {
+                builder.Append(s);
+            }
+            String result = builder.ToString();
+            return result;
+        }
+
+        public string InverterString(string Word)
+        {
+            char[] arrChar = Word.ToCharArray();
+            Array.Reverse(arrChar);
+            string invertida = new String(arrChar);
+
+            return invertida;
+        }
+
+        public List<string> ConverterEmHexadecimal(List<int> valorDecimal)
+        {
+
+            List<string> listaHexa = new List<string>();
+
+            foreach (int valor in valorDecimal)
+            {
+                if (valor == 10)
+                {
+                    listaHexa.Add("A");
+                }
+                else if (valor == 11)
+                {
+                    listaHexa.Add("B");
+                }
+                else if (valor == 12)
+                {
+                    listaHexa.Add("C");
+                }
+                else if (valor == 13)
+                {
+                    listaHexa.Add("D");
+                }
+                else if (valor == 14)
+                {
+                    listaHexa.Add("E");
+                }
+                else if (valor == 15)
+                {
+                    listaHexa.Add("F");
+                }
+                else
+                {
+                    listaHexa.Add(valor + string.Empty);
+                }
+            }
+
+            return listaHexa;
+
+        }
+
+        public double HexadecimalParaDecimal(String s)
+        {
+            int tamanho = s.Count();
+            s = s.ToUpper();
+            s = inverterString(s);
+            double d = 0;
+
+            for (int i = 0; i < tamanho; i++)
+            {
+                if (s.Substring(i, 1).Equals("A"))
+                {
+                    d = d + Convert.ToInt32(10 * Math.Pow(16, i));
+                }
+                else if (s.Substring(i, 1).Equals("B"))
+                {
+                    d = d + Convert.ToInt32(11 * Math.Pow(16, i));
+                }
+                else if (s.Substring(i, 1).Equals("C"))
+                {
+                    d = d + Convert.ToInt32(12 * Math.Pow(16, i));
+                }
+                else if (s.Substring(i, 1).Equals("D"))
+                {
+                    d = d + Convert.ToInt32(13 * Math.Pow(16, i));
+                }
+                else if (s.Substring(i, 1).Equals("E"))
+                {
+                    d = d + Convert.ToInt32(14 * Math.Pow(16, i));
+                }
+                else if (s.Substring(i, 1).Equals("F"))
+                {
+                    d = d + Convert.ToInt32(15 * Math.Pow(16, i));
+                }
+                else
+                {
+                    double a = Convert.ToInt32(s.Substring(i, 1));
+                    d = d + Convert.ToInt32(a * Math.Pow(16, i));
+                }
+            }
+
+            return d;
+        }
+
+        public double OctalParaDecimal(String o)
+        {
+            double d = 0;
+            o = inverterString(o);
+
+            for (int i = 0; i < o.Length; i++)
+            {
+                double a = Convert.ToInt32(o.Substring(i, 1));
+                d = d + Convert.ToInt32(a * Math.Pow(8, i));
+
+            }
+            return d;
+        }
+
+        public double BinarioParaDecimal(String binario)
+        {
+
+            decimal dividendo = Convert.ToDecimal(binario);
+            double d = 0;
+       
+            string binarioInvertido = inverterString(binario);
+
+            for (int i = 0; i < binarioInvertido.Length; i++)
+            {
+                d = d + (Convert.ToInt32((binarioInvertido.Substring(i, 1))) * Math.Pow(2, i));
+            }
+
+            return d;
+        }
+    }
+}
